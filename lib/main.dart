6a4813ab99f73878dfea81c166d767f7da4fafa0
Zidane Alif Putra <BSI80210@bsi.co.id>
2024-02-01 09:15:03 +0700

    update ui

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class _WeatherAppState extends State<WeatherApp> {
     getCurrentLocation();
   }
 
-  Future<void> getCurrentLocation() async {
+  void getCurrentLocation() async {
     Position position = await Geolocator.getCurrentPosition(
         desiredAccuracy: LocationAccuracy.high);
     setState(() {
@@ -38,7 +38,7 @@ class _WeatherAppState extends State<WeatherApp> {
     getWeatherData();
   }
 
-  Future<void> getWeatherData() async {
+  void getWeatherData() async {
     String apiKey = 'c36d00a1ade6f97e5f7d9861c3dff92c';
     String apiUrl =
         'https://api.openweathermap.org/data/2.5/weather?lat=$_latitude&lon=$_longitude&appid=$apiKey&units=metric';
@@ -58,7 +58,7 @@ class _WeatherAppState extends State<WeatherApp> {
 
   @override
   Widget build(BuildContext context) {
-    String googleApiKey = '';
+    String googleApiKey = 'AIzaSyAVsku3_R5bbF-Vc9dt7fnZgu_iR1BYLUM';
     String markerUrl = '$_latitude,$_longitude';
     String mapUrl =
         'https://maps.googleapis.com/maps/api/staticmap?center=$_latitude,$_longitude&zoom=14&size=400x400&markers=$markerUrl&key=$googleApiKey';
@@ -68,116 +68,101 @@ class _WeatherAppState extends State<WeatherApp> {
         appBar: AppBar(
           title: const Text('Weather App'),
         ),
-        body: FutureBuilder(
-          future: getCurrentLocation(),
-          builder: (BuildContext context, AsyncSnapshot<void> snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            } else if (snapshot.hasError) {
-              return const Center(
-                child: Text('Error occurred while fetching weather data.'),
-              );
-            } else {
-              return SingleChildScrollView(
-                child: Center(
-                  child: Container(
-                    decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                        begin: Alignment.topCenter,
-                        end: Alignment.bottomCenter,
-                        colors: [
-                          Colors.blue[200] as Color,
-                          Colors.blue[400] as Color,
-                        ],
-                      ),
+        body: SingleChildScrollView(
+          child: Center(
+            child: Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Colors.blue[200] as Color,
+                    Colors.blue[400] as Color,
+                  ],
+                ),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Latitude: $_latitude',
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontFamily: 'Roboto',
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
                     ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          'Latitude: $_latitude',
-                          style: const TextStyle(
-                            fontSize: 20,
-                            fontFamily: 'Roboto',
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                          ),
-                        ),
-                        Text(
-                          'Longitude: $_longitude',
-                          style: const TextStyle(
-                            fontSize: 20,
-                            fontFamily: 'Roboto',
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                          ),
-                        ),
-                        const SizedBox(height: 20),
-                        Text(
-                          'City: $_city',
-                          style: const TextStyle(
-                            fontSize: 20,
-                            fontFamily: 'Roboto',
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                          ),
-                        ),
-                        Text(
-                          'Weather: $_weather',
-                          style: const TextStyle(
-                            fontSize: 20,
-                            fontFamily: 'Roboto',
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                          ),
-                        ),
-                        Text(
-                          'Temperature: ${double.parse(_temperature).toStringAsFixed(0)}°C',
-                          style: const TextStyle(
-                            fontSize: 20,
-                            fontFamily: 'Roboto',
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                          ),
-                        ),
-                        const SizedBox(height: 20),
-                        Image.network(
-                          _iconUrl,
-                          width: 100,
-                          height: 100,
-                        ),
-                        const SizedBox(height: 20),
-                        Container(
-                          width: 300,
-                          height: 300,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.grey.withOpacity(0.5),
-                                spreadRadius: 5,
-                                blurRadius: 7,
-                                offset: const Offset(0, 3),
-                              ),
-                            ],
-                          ),
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(10),
-                            child: Image.network(
-                              mapUrl,
-                              fit: BoxFit.cover,
-                            ),
-                          ),
+                  ),
+                  Text(
+                    'Longitude: $_longitude',
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontFamily: 'Roboto',
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Text(
+                    'City: $_city',
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontFamily: 'Roboto',
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Text(
+                    'Weather: $_weather',
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontFamily: 'Roboto',
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Text(
+                    'Temperature: ${double.parse(_temperature).toStringAsFixed(0)}°C',
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontFamily: 'Roboto',
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Image.network(
+                    _iconUrl,
+                    width: 100,
+                    height: 100,
+                  ),
+                  const SizedBox(height: 20),
+                  Container(
+                    width: 300,
+                    height: 300,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 5,
+                          blurRadius: 7,
+                          offset: const Offset(0, 3),
                         ),
                       ],
                     ),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: Image.network(
+                        mapUrl,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
                   ),
-                ),
-              );
-            }
-          },
+                ],
+              ),
+            ),
+          ),
         ),
       ),
     );
